5583607a45d8555b639bd9bd8143e782cc920e13
AgedAnna <annabeatryz12345@gmail.com>
2024-08-20 19:50:09 -0300

    feat: test true and false

--- demo/src/test/java/MainTest.java
+++ demo/src/test/java/MainTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
@@ -23,7 +25,14 @@ public class MainTest {
     @Test
     public void ehParTest() {
         Main m = new Main();
-        Boolean res = m.ehPar(4);
-        assertEquals(true, res);
+        assertTrue(m.ehPar(2));
+
+    }
+
+    @Test
+    public void ehImparTest() {
+        Main m = new Main();
+        assertFalse(m.ehPar(3));
+
     }
 }
